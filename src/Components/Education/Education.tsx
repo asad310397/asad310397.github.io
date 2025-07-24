9871f4083fed935bceb9295316fe7c18b5948d1b
asad31 <nemo310397@gmail.com>
2025-07-23 22:03:35 -0400

    Making Projects and Education section more mobile friendly

--- src/Components/Education/Education.tsx
+++ src/Components/Education/Education.tsx
@@ -15,29 +15,36 @@ const Education = () => {
         Education
       </Typography>
       <Grid container spacing={3}>
-        <Grid size={{ md: 3, lg: 5 }}>
+        <Grid size={{ md: 6, lg: 5 }}>
           <Card className="education-card">
             <CardActionArea>
               <CardContent>
-                <Stack direction={"row"} spacing={3}>
-                  <img
-                    width="150px"
-                    height="150px"
-                    src="coding/600px-University_of_Guelph.png"
-                    alt="University Of Guelph"
-                  />
-                  <Stack direction={"column"} spacing={3}>
-                    {" "}
-                    <Typography variant="h5"> Bachelor Of Computing</Typography>
-                    <Typography variant="subtitle1">
+                <Grid container>
+                  <Grid size={{ lg: 4, sm: 6 }}>
+                    <img
+                      width="150px"
+                      height="150px"
+                      src="coding/600px-University_of_Guelph.png"
+                      alt="University Of Guelph"
+                    />
+                  </Grid>
+                  <Grid size={{ lg: 8, sm: 6 }}>
+                    <Stack direction={"column"} spacing={3}>
                       {" "}
-                      University Of Guelph
-                    </Typography>
-                    <Typography variant="body1" color="text.disabled">
-                      Sept 2015 - Dec 2019
-                    </Typography>
-                  </Stack>
-                </Stack>
+                      <Typography variant="h5">
+                        {" "}
+                        Bachelor Of Computing
+                      </Typography>
+                      <Typography variant="subtitle1">
+                        {" "}
+                        University Of Guelph
+                      </Typography>
+                      <Typography variant="body1" color="text.disabled">
+                        Sept 2015 - Dec 2019
+                      </Typography>
+                    </Stack>
+                  </Grid>
+                </Grid>
               </CardContent>
             </CardActionArea>
           </Card>
